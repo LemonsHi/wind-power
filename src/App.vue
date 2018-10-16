9b81c18d2b52181f2359cbf0e24fc9735be5c051
LemonsHi <wulemons@gmail.com>
2018-10-16 13:15:30 +0800

    'fix'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <BusinessIndicators />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import BusinessIndicators from "./components/BusinessIndicators.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    BusinessIndicators
   }
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: absolute;
+  height: 100%;
+  width: 100%;
+
+  // background: rgba(0, 0, 0, 0.7);
 }
 </style>
---
+++ src/components/BusinessIndicators.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="business-indicators-panel">
+    <div class="box-header">
+      <i class="iconfont">&#xe60d;</i>
+      <span>数据汇总</span>
+      <div class="box-header-button iconfont">&#xe611;</div>
+    </div>
+    <div class="box-body">
+
+    </div>
+    <div class="box-footer">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped lang="scss">
+.business-indicators-panel {
+  position: absolute;
+  bottom: 0px;
+  right: 0px;
+  width: 300px;
+  height: 500px;
+  background-color: rgba(0, 0, 0, 0.1);
+
+  .box-header {
+    width: 100%;
+    height: 40px;
+    background-color: rgba(0, 0, 0, 0.6);
+
+    color: #ffffff;
+    line-height: 40px;
+    font-size: 18px;
+    i {
+      margin: 0 10px;
+      font-size: 20px;
+    }
+    span {
+      font-family: '黑体'
+    }
+
+    .box-header-button {
+      float: right;
+      width: 60px;
+      text-align: center;
+      border-left: 1px solid #eee;
+      border-right: 1px solid rgba(0, 0, 0, 0.6);
+      transition: all 0.3s;
+      &:hover {
+        background-color: rgba(0, 0, 0, 0.8)
+      }
+    }
+  }
+}
+</style>
